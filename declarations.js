d2be0dc6ff5957bc744a8dffb466cacdd045ae22
somotto <stephenonyango>
2024-09-30 14:22:57 +0300

    freeze  declaration2

--- declarations.js
+++ declarations.js
@@ -1,27 +1,24 @@
 const escapeStr = "` \\ / \"'"
 const arr = [4, '2']
+Object.freeze(arr)
 const obj = {
     str: 'Hello',
     num: 20,
     bool: true,
     undef: undefined,
 }
+Object.freeze(obj)
 const nested = {
     arr: [4, undefined, '2'],
     obj: {
-        str: 'world',
-        num: 10,
+        str: 'Hello',
+        num: 20,
         bool: true
     }
 }
+Object.freeze(nested)
 Object.freeze(nested.arr)
 Object.freeze(nested.obj)
-Object.freeze(arr)
-Object.freeze(obj)
 
-console.log(arr)
-console.log(obj)
-console.log(nested)
-console.log(escapeStr)
 
 
