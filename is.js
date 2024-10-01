c4250fa33e0d9ab9542e4ac39210de3d7185b0b2
somotto <stephenonyango4783@gmail.com>
2024-10-01 15:20:54 +0300

    feat: initial Is function

---
+++ is.js
@@ -0,0 +1,46 @@
+const is = {};
+
+is.num = function(value) {
+    return typeof value === 'number' && !is.nan(value);
+};
+
+is.nan = function(value) {
+    return typeof value === 'number' && isNaN(value);
+};
+
+is.str = function(value) {
+    return typeof value === 'string';
+};
+
+is.bool = function(value) {
+    return typeof value === 'boolean';
+};
+is.undef = function(value) {
+    return typeof value === 'undefined';
+};
+
+is.def = function(value) {
+    return typeof value !== 'undefined';
+};
+
+is.arr = function(value) {
+    return Array.isArray(value);
+};
+
+is.obj = function(value) {
+    return value !== null && typeof value === 'object';
+};
+
+is.fun = function(value) {
+    return typeof value === 'function';
+};
+
+is.truthy = function(value) {
+    return !!value;
+};
+
+is.falsy = function(value) {
+    return !value;
+};
+
+
