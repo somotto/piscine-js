c182a0440857d385c8c353e96931e8de3d24e468
somotto <stephenonyango4783@gmail.com>
2024-10-01 17:48:40 +0300

    feat: initial physics function4

--- physics.js
+++ physics.js
@@ -1,22 +1,15 @@
 const getAcceleration = (params) => {
-    const { f, m, Δv, Δt, t, d } = params;
-
-    const accelerations = [];
-
-    if (m !== undefined && m !== 0) {
-        const a1 = f / m;
-        accelerations.push(a1);
+    const { f, m, Δv, Δt, d, t } = params;
+    if (f !== undefined && m !== undefined) {
+        return f / m; 
+    } 
+    else if (Δv !== undefined && Δt !== undefined) {
+        return Δv / Δt; 
+    } 
+    else if (d !== undefined && t !== undefined) {
+        return (2 * d) / (t * t); 
+    } 
+    else {
+        return 'impossible';
     }
-
-    if (Δv !== undefined && Δt !== undefined && Δt !== 0) {
-        const a2 = Δv / Δt;
-        accelerations.push(a2);
-    }
-
-    if (d !== undefined && t !== undefined && t !== 0) {
-        const a3 = (2 * d) / (t * t);
-        accelerations.push(a3);
-    }
-
-    return accelerations.length === 0 ? "impossible" : accelerations;
 };
