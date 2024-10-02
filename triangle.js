d16be348062bc30cc2373890623248a2c19c7b30
somotto <stephenonyango4783@gmail.com>
2024-10-03 01:52:25 +0300

    feat: create tringle

---
+++ triangle.js
@@ -0,0 +1,14 @@
+const triangle = (char, height) => {
+    let result = '';
+    for (let i = 1; i <= height; i++) {
+      result += char.repeat(i);
+      if (i < height) {
+        result += '\n';
+      }
+    }
+    return result;
+  };
+  
+  
+//   console.log(triangle('*', 5));
+  
