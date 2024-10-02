3ce187e4cebdb179983dcc5e562e6a978310d845
somotto <stephenonyango4783@gmail.com>
2024-10-02 21:18:17 +0300

    chore: update keep function

--- keep-cut.js
+++ keep-cut.js
@@ -8,4 +8,9 @@ const keepFirst = (str) => str.slice(0, 2);
 
 const keepLast = (str) => str.slice(-2);
 
-const keepFirstLast = (str) => str.slice(0, 2) + str.slice(-2);
+const keepFirstLast = (str) => {
+    if (str.length <= 4) {
+        return str;
+    }
+    return str.slice(0, 2) + str.slice(-2);
+};
