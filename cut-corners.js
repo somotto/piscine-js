4bce6640cf22ef71a24577dbae76ded725ff3f4b
somotto <stephenonyango4783@gmail.com>
2024-10-02 22:20:21 +0300

    feat: add round functions2

--- cut-corners.js
+++ cut-corners.js
@@ -1,22 +1,32 @@
 const round = (num) => {
-    const intPart = trunc(num);
-    const fraction = Math.abs(num - intPart);
-    if (fraction < 0.5) return intPart;
-    return num > 0 ? intPart + 1 : intPart - 1;
+    const intPart = num - (num % 1);
+    const decimalPart = num - intPart;
+
+    if (num > 0) {
+        return decimalPart < 0.5 ? intPart : intPart + 1;
+    } else {
+        return decimalPart > -0.5 ? intPart : intPart - 1;
+    }
 };
-  
-const floor = (num) => {
-    const intPart = trunc(num);
-    return num >= 0 || num === intPart ? intPart : intPart - 1;
-};
-  
+
 const ceil = (num) => {
-    const intPart = trunc(num);
-    return num <= 0 || num === intPart ? intPart : intPart + 1;
+    return (num > 0) ? (num - (num % 1) + (num % 1 > 0 ? 1 : 0))
+        : (num - (num % 1));
 };
-  
+
+const floor = (num) => {
+    return (num > 0) ? (num - (num % 1))
+        : (num - (num % 1) - (num % 1 < 0 ? 1 : 0));
+};
+
 const trunc = (num) => {
-    return num < 0 ? -Math.floor(-num) : Math.floor(num);
+    return (num > 0) ? (num - (num % 1))
+        : (num - (num % 1));
 };
 
-
+// // Usage
+// const nums = [3.7, -3.7, 3.1, -3.1];
+// console.log(nums.map(round));  // [4, -4, 3, -3]
+// console.log(nums.map(floor));  // [3, -4, 3, -4]
+// console.log(nums.map(trunc));  // [3, -3, 3, -3]
+// console.log(nums.map(ceil));   // [4, -3, 4, -3]
