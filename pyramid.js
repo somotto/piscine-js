795818feb5522d57e6dc8e47e29b693f59905cff
somotto <stephenonyango4783@gmail.com>
2024-10-03 01:56:57 +0300

    feat: creat pyramid

---
+++ pyramid.js
@@ -0,0 +1,15 @@
+const pyramid = (char, height) => {
+    let result = '';
+    for (let i = 1; i <= height; i++) {
+        const spaces = ' '.repeat(height - i);
+        const chars = char.repeat(2 * i - 1);
+        result += spaces + chars;
+        if (i < height) {
+            result += '\n';
+        }
+    }
+    return result;
+};
+
+
+//   console.log(pyramid('*', 5));
