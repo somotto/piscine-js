8c37916ab4e82dd13aff15dc7d7f4082faf24bc2
somotto <stephenonyango4783@gmail.com>
2024-10-03 03:37:10 +0300

    feat:  find expression5

--- find-expression.js
+++ find-expression.js
@@ -1,26 +1,32 @@
 // const add4 = '+4';
 // const mul2 = '*2';
-const findExpression = (number) => {
-    for (let i = 0; i < 100000; i++) {
-      let result = 1;
-      let sequence = "1";
-      while (result <= number) {
-        if (result === number) {
-          return sequence;
-        }
-        if (Math.random() < 0.4 + 0.1) {
-          result += 4;
-          sequence += " + 4"; 
-        } else {
-          result *= 2;
-          sequence += " * 2"; 
-        }
-      }
-    }
-    return undefined;
-  };
 
-// // Example usage:
+function findExpression(number, current = 1, sequence = "1") {
+
+  if (current === number) {
+    return sequence;
+  }
+
+  if (current > number) {
+    return undefined;
+  }
+
+
+  const addPath = findExpression(number, current + 4, sequence + " " + add4);
+  if (addPath) {
+    return addPath;  // If adding 4 leads to a solution, return it.
+  }
+
+
+  const mulPath = findExpression(number, current * 2, sequence + " " + mul2);
+  if (mulPath) {
+    return mulPath;  
+  }
+
+  return undefined;
+}
+
+
 // console.log(findExpression(8)); // Expected Output: "1 *2 *2 +4"
 // console.log(findExpression(9)); // Expected Output: "1 *2 +4"
 // console.log(findExpression(15)); // Expected Output: "1 *2 +4 +4"
