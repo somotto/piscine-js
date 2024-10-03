a722b3754c1be580d4e9f856b4a999238aeccc6b
somotto <stephenonyango4783@gmail.com>
2024-10-03 05:54:47 +0300

    feat: add sums function

---
+++ sums.js
@@ -0,0 +1,18 @@
+const sums = (n) => {
+  const result = [];
+  
+  const findPartitions = (target, currentPartition, lastNumber) => {
+      if (target === 0) {
+          result.push([...currentPartition]);
+          return;
+      }
+      for (let i = lastNumber; i <= target; i++) {
+          currentPartition.push(i);
+          findPartitions(target - i, currentPartition, i);
+          currentPartition.pop();
+      }
+  };
+  
+  findPartitions(n, [], 1);
+  return result.slice(0, -1);
+};
