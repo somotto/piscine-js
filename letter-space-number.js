07e4f0f6f2fba9044220a795fb8989f059fb0c3d
somotto <stephenonyango4783@gmail.com>
2024-10-04 17:44:39 +0300

    match all characters

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,9 +1,10 @@
-const letterSpaceNumber = str => {
-    return Array.from(str.matchAll(/([a-zA-Z]) (\d)(?=\s|$)/g))
-        .map(match => `${match[1]} ${match[2]}`);
-};
-
-// console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
-
-//, ['s 8', 'r 9'])
-console.log(letterSpaceNumber('example 1, example 20'))
+const letterSpaceNumber = (str) => {
+    const regex = /[a-zA-Z] \d(?!\d)(?![a-zA-Z])/g;
+    return str.match(regex) || [];
+  };
+  
+//   // Test cases
+//   console.log(letterSpaceNumber('example 1, example 20'));
+//   console.log(letterSpaceNumber('a 1 b 2 c 3 d 4 e5 f 6'));
+//   console.log(letterSpaceNumber('string 22 string'));
+//   console.log(letterSpaceNumber(''));
