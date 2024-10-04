81b347ef177a4fcfd2916ca9467a970fdae344b9
somotto <stephenonyango4783@gmail.com>
2024-10-04 18:46:35 +0300

    feat: get url

--- greedy-url.js
+++ greedy-url.js
@@ -1,17 +1,18 @@
 const getURL = (dataSet) => {
     const urlRegex = /(https?:\/\/[^\s]+)/g;
     return dataSet.match(urlRegex) || [];
-  };
+};
   
-  const greedyQuery = (dataSet) => {
+const greedyQuery = (dataSet) => {
     const greedyRegex = /(https?:\/\/[^\s]+\?[^\s]*(&[^\s]*){2,})/g;
     return dataSet.match(greedyRegex) || [];
-  };
+};
   
-  const notSoGreedy = (dataSet) => {
-    const notSoGreedyRegex = /(https?:\/\/[^\s]+\?[^\s]*(&[^\s]*){1,2}(?!&))/g;
+const notSoGreedy = (dataSet) => {
+    const notSoGreedyRegex = /(https?:\/\/[^\s]+\?[^&\s]+(&[^&\s]+){1,2})(?!&)/g;
     return dataSet.match(notSoGreedyRegex) || [];
-  };
+};
+  
   
   // Example usage
 //   const dataSet = `qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you`;
