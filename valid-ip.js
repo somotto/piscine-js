4807734a190444df999d3dcf2db2aadd494edeae
somotto <stephenonyango4783@gmail.com>
2024-10-05 20:40:11 +0300

    feat: find valid ip

--- valid-ip.js
+++ valid-ip.js
@@ -1,5 +1,5 @@
 const findIP = (str) => {
-    const ipRegex = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?::(?:[1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?\b/g;
+    const ipRegex = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?::(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{0,3}))?\b/g;
     
     const results = str.match(ipRegex) || [];
     
@@ -8,6 +8,10 @@ const findIP = (str) => {
       const octets = address.split('.');
       
       if (octets.some(octet => octet.length > 1 && octet[0] === '0')) {
+        return false;
+      }
+      
+      if (octets.some(octet => isNaN(parseInt(octet)) || parseInt(octet) > 255)) {
         return false;
       }
       
