1b98dcac467ff7e2f24b5106b7aab3ccf61eb079
somotto <stephenonyango4783@gmail.com>
2024-10-07 17:31:23 +0300

    feat: update

--- get-some-time.js
+++ get-some-time.js
@@ -13,16 +13,16 @@ const firstDayWeek = (week, year) => {
     firstDayOfWeek.setDate(firstMonday.getDate() + (week - 1) * 7);
 
     if (firstDayOfWeek.getFullYear() < year) {
-        return `01-01-${year}`;
+        return `01-01-${year.padStart(4,'0')}`;
     }
 
     const day = String(firstDayOfWeek.getDate()).padStart(2, '0');
     const month = String(firstDayOfWeek.getMonth() + 1).padStart(2, '0');
-    const resultYear = firstDayOfWeek.getFullYear();
+    const resultYear = firstDayOfWeek.getFullYear(4, '0');
 
     return `${day}-${month}-${resultYear}`;
 };
 
-// Example usage:
-console.log(firstDayWeek(1, "2024"));
-console.log(firstDayWeek(10, "2023"));
+// // Example usage:
+// console.log(firstDayWeek(1, "2024"));
+// console.log(firstDayWeek(10, "2023"));
