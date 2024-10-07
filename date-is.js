f7430a56171e86a34069a1c2df3d4813a0276d7c
somotto <stephenonyango4783@gmail.com>
2024-10-07 15:19:44 +0300

    feat: date-is functions

---
+++ date-is.js
@@ -0,0 +1,19 @@
+function isValid(date) {
+    return date instanceof Date && !isNaN(date);
+  }
+  
+  function isAfter(date1, date2) {
+    return isValid(date1) && isValid(date2) && date1 > date2;
+  }
+  
+  function isBefore(date1, date2) {
+    return isValid(date1) && isValid(date2) && date1 < date2;
+  }
+  
+  function isFuture(date) {
+    return isValid(date) && date > new Date();
+  }
+  
+  function isPast(date) {
+    return isValid(date) && date < new Date();
+  }
