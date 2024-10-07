9ad75a8a9bb1a1c4ff7e4541a68a58c85024b502
somotto <stephenonyango4783@gmail.com>
2024-10-07 17:07:24 +0300

    feat: update

--- get-some-time.js
+++ get-some-time.js
@@ -2,6 +2,7 @@ const firstDayWeek = (weekNumber, yearString) => {
     const year = parseInt(yearString, 10);
     
     const januaryFirst = new Date(year, 0, 1); 
+
     const dayOfWeek = januaryFirst.getDay(); 
     const daysToFirstMonday = (dayOfWeek === 0) ? 1 : (8 - dayOfWeek); 
     const firstMonday = new Date(januaryFirst);
@@ -10,16 +11,18 @@ const firstDayWeek = (weekNumber, yearString) => {
     const firstDayOfWeek = new Date(firstMonday);
     firstDayOfWeek.setDate(firstMonday.getDate() + (weekNumber - 1) * 7);
     
-    if (firstDayOfWeek < januaryFirst) {
+   
+    if (weekNumber === 1 && firstDayOfWeek < januaryFirst) {
         return `01-01-${year}`;
     }
     
     const dd = String(firstDayOfWeek.getDate()).padStart(2, '0');
-    const mm = String(firstDayOfWeek.getMonth() + 1).padStart(2, '0');
+    const mm = String(firstDayOfWeek.getMonth() + 1).padStart(2, '0'); 
     const yyyy = firstDayOfWeek.getFullYear();
     
     return `${dd}-${mm}-${yyyy}`;
 };
 
+// console.log(firstDayWeek(1, "1000")); 
 // console.log(firstDayWeek(1, "2023"));  
 // console.log(firstDayWeek(53, "2023")); 
