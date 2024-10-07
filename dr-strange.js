2e28679de534f0dc216d7f998ef4a57ecdd58d42
somotto <stephenonyango4783@gmail.com>
2024-10-07 14:57:57 +0300

    feat: add dr-strange functions

--- dr-strange.js
+++ dr-strange.js
@@ -1,25 +1,38 @@
 function addWeek(date) {
-    const newWeekDays = [
-        "Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday",
-        "secondMonday", "secondTuesday", "secondWednesday",
-        "secondThursday", "secondFriday", "secondSaturday", "secondSunday"
+    const epochDate = new Date('0001-01-01');
+
+    const diffTime = Math.abs(date - epochDate);
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    const dayOfWeek = diffDays % 14;
+
+    const days = [
+        'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+        'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'
     ];
 
-    const epoch = new Date(1, 0, 1);
-
-    const diffInTime = date - epoch;
-    const diffInDays = Math.floor(diffInTime / (1000 * 60 * 60 * 24));
-    const weekPosition = diffInDays % 14;
-
-    return newWeekDays[weekPosition];
-};
+   
+    return days[dayOfWeek];
+}
 
 function timeTravel({ date, hour, minute, second }) {
     const newDate = new Date(date);
-    if (hour !== undefined) newDate.setHours(hour);
-    if (minute !== undefined) newDate.setMinutes(minute);
-    if (second !== undefined) newDate.setSeconds(second);
+
+    newDate.setHours(hour);
+    newDate.setMinutes(minute);
+    newDate.setSeconds(second);
 
     return newDate;
-};
+}
+
+console.log(addWeek(new Date('0001-01-01')));
+console.log(addWeek(new Date('0001-01-08')));
+
+const traveledDate = timeTravel({
+    date: new Date('2020-05-29 23:25:22'),
+    hour: 21,
+    minute: 22,
+    second: 22,
+});
+
+console.log(traveledDate.toString());
