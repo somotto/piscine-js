cfffc456bcf796ecbcedbe62191b42a8be80d88e
somotto <stephenonyango4783@gmail.com>
2024-10-08 17:43:41 +0300

    use filters

---
+++ using-filter.js
@@ -0,0 +1,17 @@
+const filterShortStateName = (arr) => arr.filter(state => state.length < 7);
+
+const filterStartVowel = (arr) => arr.filter(state => /^[aeiou]/i.test(state));
+
+const filter5Vowels = (arr) => arr.filter(state => (state.match(/[aeiou]/gi) || []).length >= 5);
+
+const filter1DistinctVowel = (arr) => arr.filter(state => {
+    const vowels = new Set(state.match(/[aeiou]/gi));
+    return vowels.size === 1;
+});
+
+const multiFilter = (arr) => arr.filter(obj => 
+    obj.capital.length >= 8 &&
+    !/^[aeiou]/i.test(obj.name) &&
+    /[aeiou]/i.test(obj.tag) &&
+    obj.region !== 'South'
+);
