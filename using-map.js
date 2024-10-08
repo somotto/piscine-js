2369a98ace041d91f5fb438095b397693f3ba752
somotto <stephenonyango4783@gmail.com>
2024-10-08 16:33:55 +0300

    use map

---
+++ using-map.js
@@ -0,0 +1,54 @@
+function citiesOnly(arr) {
+    return arr.map(obj => obj.city);
+}
+
+function upperCasingStates(arr) {
+    return arr.map(state => 
+        state.split(' ')
+             .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+             .join(' ')
+    );
+}
+
+function fahrenheitToCelsius(arr) {
+    return arr.map(temp => {
+        const fahrenheit = parseInt(temp); 
+        const celsius = Math.floor((fahrenheit - 32) * 5 / 9);
+        return `${celsius}°C`;
+    });
+}
+
+function trimTemp(arr) {
+    return arr.map(obj => ({
+        city: obj.city,
+        temperature: obj.temperature.trim().replace(/\s+/g, '')
+    }));
+}
+
+
+function tempForecasts(arr) {
+    return arr.map(obj => {
+        const fahrenheit = parseInt(obj.temperature); 
+        const celsius = Math.floor((fahrenheit - 32) * 5 / 9);
+        return `${celsius}°Celsius in ${obj.city}, ${obj.state.charAt(0).toUpperCase() + obj.state.slice(1)}`;
+    });
+}
+
+
+// console.log(citiesOnly([
+//     { city: 'Los Angeles', temperature: '  101 °F   ' },
+//     { city: 'San Francisco', temperature: ' 84 ° F   ' },
+// ])); 
+
+// console.log(upperCasingStates(['alabama', 'new jersey'])); 
+
+// console.log(fahrenheitToCelsius(['68°F', '59°F', '25°F'])); 
+
+// console.log(trimTemp([
+//     { city: 'Los Angeles', temperature: '  101 °F   ' },
+//     { city: 'San Francisco', temperature: ' 84 ° F   ' },
+// ])); 
+
+// console.log(tempForecasts([
+//     { city: 'Pasadena', temperature: ' 101 °F', state: 'california', region: 'West' },
+// ])); 
