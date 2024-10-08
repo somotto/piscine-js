ee4e3b349d0644e529ba6e592ac242c37a209fac
somotto <stephenonyango4783@gmail.com>
2024-10-08 11:49:26 +0300

    update

--- get-some-time.js
+++ get-some-time.js
@@ -12,11 +12,13 @@ const firstDayWeek = (week, year) => {
       dateStr = year + "-" + month[0] + "-" + month[1] + "T02:39:49";
   }
   if (week === 2 && year === '2017') return "02-01-2017"
-  let date = dateStr === undefined ? new Date(year, 0, 1 + (week - 1) * 7, 2) : new Date(dateStr);
-  date.setHours(0, 0, 0, 0);
+  let date = 
+  dateStr === undefined ? new Date(year, 0, 1 + (week - 1) * 7, 2) 
+  : new Date(dateStr);
+  date.setHours(0,0,0,0);
   let d1 = new Date(date);
   date.setDate(date.getDate() - date.getDay() + 1);
-  if (date - getFullYear().toString() !== year) {
+  if (date.getFullYear().toString() !== year) {
       date = d1;
   }
   return format_date(date)
