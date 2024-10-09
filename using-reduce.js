a8119d8d3b2b9e56b56e34c861a8b169f592d3ab
somotto <stephenonyango4783@gmail.com>
2024-10-09 13:36:40 +0300

    use reduce

--- using-reduce.js
+++ using-reduce.js
@@ -1,11 +1,14 @@
 const adder = (numbers, initialValue = 0) => {
     return numbers.reduce((sum, num) => sum + num, initialValue);
 };
-const sumOrMul = (numbers, initialValue = 1) => {
-    return numbers.reduce((result, num) => {
-        return num % 2 === 0 ? result * num : result + num;
+const sumOrMul = (numbers, initialValue) => {
+    return numbers.reduce((result, num, index) => {
+      if (index === 0 && initialValue === undefined) {
+        return num;
+      }
+      return num % 2 === 0 ? result * num : result + num;
     }, initialValue);
-};
+  };
 const funcExec = (functions, initialValue) => {
     return functions.reduce((result, func) => {
         return func(result);
