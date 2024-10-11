39d1576f133e85cb35c81c9ab2bd25fd6442ce43
somotto <stephenonyango4783@gmail.com>
2024-10-11 20:19:17 +0300

    add pimp style

---
+++ pimp-my-style.js
@@ -0,0 +1,25 @@
+import { styles } from './pimp-my-style.data.js';
+
+let currentIndex = -1;
+const button = document.querySelector('.button');
+
+export function pimp() {
+
+    if (currentIndex < styles.length - 1) {
+
+        currentIndex++;
+        button.classList.toggle('unpimp');
+        button.classList.add(styles[currentIndex]);
+    } else {
+
+        button.classList.toggle('unpimp');
+
+        button.classList.remove(styles[currentIndex]);
+        currentIndex--;
+    }
+
+    if (currentIndex < -1) {
+        currentIndex = -1;
+        button.classList.toggle('unpimp');
+    }
+}
