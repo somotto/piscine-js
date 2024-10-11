36a6c64b02298e26f4970ec494e94773df29ba51
somotto <stephenonyango4783@gmail.com>
2024-10-11 20:51:04 +0300

    add pimp style

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -3,26 +3,24 @@ import { styles } from './pimp-my-style.data.js';
 let currentIndex = 0;
 let isRemoving = false;
 
-function pimp() {
-  const button = document.querySelector('.button');
-  
-  if (!isRemoving) {
-    if (currentIndex < styles.length) {
-      button.classList.add(styles[currentIndex]);
-      currentIndex++;
-    } else {
-      isRemoving = true;
-      button.classList.toggle('unpimp', true);
-    }
-  } else {
-    if (currentIndex > 0) {
-      currentIndex--;
-      button.classList.remove(styles[currentIndex]);
-    } else {
-      isRemoving = false;
-      button.classList.toggle('unpimp', false);
-    }
-  }
-}
+export function pimp() {
+    const button = document.querySelector('.button');
 
-document.addEventListener('click', pimp);
+    if (!isRemoving) {
+        if (currentIndex < styles.length) {
+            button.classList.add(styles[currentIndex]);
+            currentIndex++;
+        } else {
+            isRemoving = true;
+            button.classList.add('unpimp');
+        }
+    } else {
+        if (currentIndex > 0) {
+            currentIndex--;
+            button.classList.remove(styles[currentIndex]);
+        } else {
+            isRemoving = false;
+            button.classList.remove('unpimp');
+        }
+    }
+}
