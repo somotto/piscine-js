f92298f4224228d50f71a55ced867caa8de8f6f0
somotto <stephenonyango4783@gmail.com>
2024-10-12 16:05:51 +0300

    add mouse trap function

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,52 +1,58 @@
 let lastCircle = null;
 let box = null;
+let isInsideBox = false;
 
 export function createCircle() {
-    document.addEventListener('click', (event) => {
-        const circle = document.createElement('div');
-        circle.className = 'circle';
-        circle.style.left = `${event.clientX - 25}px`;
-        circle.style.top = `${event.clientY - 25}px`;
-        circle.style.background = 'white';
-        document.body.appendChild(circle);
-        lastCircle = circle;
-    });
+  document.addEventListener('click', (event) => {
+    const circle = document.createElement('div');
+    circle.className = 'circle';
+    circle.style.left = `${event.clientX - 25}px`;
+    circle.style.top = `${event.clientY - 25}px`;
+    circle.style.background = 'white';
+    document.body.appendChild(circle);
+    lastCircle = circle;
+    isInsideBox = false;
+  });
 }
 
 export function moveCircle() {
-    document.addEventListener('mousemove', (event) => {
-        if (lastCircle) {
-            const circleRect = lastCircle.getBoundingClientRect();
-            const boxRect = box.getBoundingClientRect();
+  document.addEventListener('mousemove', (event) => {
+    if (lastCircle) {
+      const circleRect = lastCircle.getBoundingClientRect();
+      const boxRect = box.getBoundingClientRect();
 
-            let newX = event.clientX - 25;
-            let newY = event.clientY - 25;
+      let newX = event.clientX - 25;
+      let newY = event.clientY - 25;
+      const insideX = newX > boxRect.left && newX + 50 < boxRect.right;
+      const insideY = newY > boxRect.top && newY + 50 < boxRect.bottom;
+      const isInside = insideX && insideY;
 
-            if (
-                circleRect.left > boxRect.left &&
-                circleRect.right < boxRect.right &&
-                circleRect.top > boxRect.top &&
-                circleRect.bottom < boxRect.bottom
-            ) {
-                lastCircle.style.background = 'var(--purple)';
-                newX = Math.max(boxRect.left + 1, Math.min(newX, boxRect.right - 51));
-                newY = Math.max(boxRect.top + 1, Math.min(newY, boxRect.bottom - 51));
-            } else {
-                lastCircle.style.background = 'white';
-            }
+      if (isInside) {
 
-            lastCircle.style.left = `${newX}px`;
-            lastCircle.style.top = `${newY}px`;
-        }
-    });
+        lastCircle.style.background = 'var(--purple)';
+        isInsideBox = true;
+      } else if (!isInsideBox) {
+        lastCircle.style.background = 'white';
+      }
+
+      if (isInsideBox) {
+        newX = Math.max(boxRect.left + 1, Math.min(newX, boxRect.right - 51));
+        newY = Math.max(boxRect.top + 1, Math.min(newY, boxRect.bottom - 51));
+      }
+
+      lastCircle.style.left = `${newX}px`;
+      lastCircle.style.top = `${newY}px`;
+    }
+  });
 }
-export function setBox() {
-    box = document.createElement('div');
-    box.className = 'box';
-    document.body.appendChild(box);
 
-    const centerX = window.innerWidth / 2 - box.offsetWidth / 2;
-    const centerY = window.innerHeight / 2 - box.offsetHeight / 2;
-    box.style.left = `${centerX}px`;
-    box.style.top = `${centerY}px`;
+export function setBox() {
+  box = document.createElement('div');
+  box.className = 'box';
+  document.body.appendChild(box);
+
+  const centerX = window.innerWidth / 2 - box.offsetWidth / 2;
+  const centerY = window.innerHeight / 2 - box.offsetHeight / 2;
+  box.style.left = `${centerX}px`;
+  box.style.top = `${centerY}px`;
 }
