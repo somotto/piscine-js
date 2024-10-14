5e09cb268285d85ece88b902858ac326ba660af7
somotto <stephenonyango4783@gmail.com>
2024-10-14 18:13:19 +0300

    feat: merge two objects

---
+++ fusion.js
@@ -0,0 +1,37 @@
+function fusion(obj1, obj2) {
+    const result = {};
+
+    const getAllKeys = (o1, o2) => [...new Set([...Object.keys(o1), ...Object.keys(o2)])];
+
+    for (const key of getAllKeys(obj1, obj2)) {
+
+        if (key in obj1 && key in obj2) {
+
+            if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+                result[key] = [...obj1[key], ...obj2[key]];
+            }
+
+            else if (typeof obj1[key] === 'string' && typeof obj2[key] === 'string') {
+                result[key] = `${obj1[key]} ${obj2[key]}`.trim();
+            }
+            else if (typeof obj1[key] === 'number' && typeof obj2[key] === 'number') {
+                result[key] = obj1[key] + obj2[key];
+            }
+
+            else if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object' && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key])) {
+                result[key] = fusion(obj1[key], obj2[key]);
+            }
+            else {
+                result[key] = obj2[key];
+            }
+        }
+        else if (key in obj1) {
+            result[key] = obj1[key];
+        }
+        else {
+            result[key] = obj2[key];
+        }
+    }
+
+    return result;
+}
