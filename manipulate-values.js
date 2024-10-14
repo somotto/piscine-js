5aef2d7c10f11d4ac596c06f5fd53bc7ff931b4e
somotto <stephenonyango4783@gmail.com>
2024-10-14 19:04:24 +0300

    feat: use filter, map and reduce functions

--- manipulate-values.js
+++ manipulate-values.js
@@ -10,6 +10,15 @@ function mapValues(obj, mapFn) {
     );
 }
 
-function reduceValues(obj, reduceFn, initialValue) {
-    return Object.values(obj).reduce(reduceFn, initialValue);
+function reduceValues(obj, callback, initialValue) {
+    const values = Object.values(obj);
+    
+    if (initialValue === undefined) {
+        if (values.length === 0) {
+            throw new TypeError('Reduce of empty object with no initial value');
+        }
+        return values.slice(1).reduce(callback, values[0]);
+    }
+    
+    return values.reduce(callback, initialValue);
 }
