b59df007a54fc89ff2564e39d233e4231269c0de
somotto <stephenonyango4783@gmail.com>
2024-10-14 21:33:20 +0300

    feat: manipulate entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,49 +1,51 @@
 const filterEntries = (obj, predicate) => {
     return Object.fromEntries(
-      Object.entries(obj).filter(entry => predicate(entry))
+        Object.entries(obj).filter(entry => predicate(entry))
     );
-  };
-  
-  const mapEntries = (obj, mapper) => {
+};
+
+const mapEntries = (obj, mapper) => {
     return Object.fromEntries(
-      Object.entries(obj).map(entry => mapper(entry))
+        Object.entries(obj).map(entry => mapper(entry))
     );
-  };
-  
-  const reduceEntries = (obj, reducer, initialValue) => {
+};
+
+const reduceEntries = (obj, reducer, initialValue) => {
     return Object.entries(obj).reduce(
-      (acc, entry) => reducer(acc, entry),
-      initialValue
+        (acc, entry) => reducer(acc, entry),
+        initialValue
     );
-  };
-  
-  const totalCalories = (cart) => {
-    return reduceEntries(
-      cart,
-      (total, [item, grams]) => {
-        return total + (nutritionDB[item].calories * grams) / 100;
-      },
-      0
+};
+
+const totalCalories = (cart) => {
+    return parseFloat(
+        reduceEntries(
+            cart,
+            (total, [item, grams]) => {
+                return total + (nutritionDB[item].calories * grams) / 100;
+            },
+            0
+        ).toFixed(1)
     );
-  };
-  
-  const lowCarbs = (cart) => {
+};
+
+const lowCarbs = (cart) => {
     return filterEntries(
-      cart,
-      ([item, grams]) => (nutritionDB[item].carbs * grams) / 100 < 50
+        cart,
+        ([item, grams]) => (nutritionDB[item].carbs * grams) / 100 < 50
     );
-  };
-  
-  const cartTotal = (cart) => {
+};
+
+const cartTotal = (cart) => {
     return mapEntries(cart, ([item, grams]) => {
-      const itemNutrition = nutritionDB[item];
-      const scaledNutrition = Object.fromEntries(
-        Object.entries(itemNutrition).map(([nutrient, value]) => [
-          nutrient,
-          (value * grams) / 100,
-        ])
-      );
-      return [item, scaledNutrition];
+        const itemNutrition = nutritionDB[item];
+        const scaledNutrition = Object.fromEntries(
+            Object.entries(itemNutrition).map(([nutrient, value]) => [
+                nutrient,
+                (value * grams) / 100,
+            ])
+        );
+        return [item, scaledNutrition];
     });
-  };
-  
+};
+
