40be1e887378b01c66815ba59fb62f433028df85
somotto <stephenonyango4783@gmail.com>
2024-10-15 13:05:27 +0300

    feat implement replica function

--- replica.js
+++ replica.js
@@ -1,20 +1,42 @@
 function replica(target, ...sources) {
-    const isObject = (item) => item && typeof item === 'object' && !Array.isArray(item);
+    const isPlainObject = (item) =>
+        item && typeof item === 'object' && Object.getPrototypeOf(item) === Object.prototype;
+
     const deepCopy = (target, source) => {
         for (const key in source) {
             if (Object.prototype.hasOwnProperty.call(source, key)) {
-                if (isObject(source[key])) {
-                    target[key] = deepCopy(Object.create(Object.getPrototypeOf(source[key])), source[key]);
-                } else if (Array.isArray(source[key])) {
-                    target[key] = source[key].map(item =>
-                        isObject(item) ? deepCopy({}, item) : item
+                const value = source[key];
+                if (isPlainObject(value)) {
+                    target[key] = deepCopy({}, value);
+                } else if (Array.isArray(value)) {
+                    target[key] = value.map(item =>
+                        isPlainObject(item) ? deepCopy({}, item) : copySpecialTypes(item)
                     );
                 } else {
-                    target[key] = source[key];
+                    target[key] = copySpecialTypes(value);
                 }
             }
         }
         return target;
+    };
+
+    const copySpecialTypes = (item) => {
+        if (item instanceof RegExp) {
+            return new RegExp(item);
+        } else if (item instanceof Date) {
+            return new Date(item);
+        } else if (typeof item === 'function') {
+            return item;
+        } else if (item instanceof Map) {
+            return new Map(item);
+        } else if (item instanceof Set) {
+            return new Set(item);
+        } else if (ArrayBuffer.isView(item)) {
+            return item.slice();
+        } else if (item instanceof ArrayBuffer) {
+            return item.slice(0);
+        }
+        return item;
     };
 
     sources.forEach(source => {
