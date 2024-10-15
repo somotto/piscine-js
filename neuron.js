292c539d3429bf9ee0383cf51b06343d22d3c8d0
somotto <stephenonyango4783@gmail.com>
2024-10-15 15:39:17 +0300

    add neuron function

--- neuron.js
+++ neuron.js
@@ -2,10 +2,8 @@ function neuron(inputArray) {
     if (inputArray.length === 0) {
         return {};
     }
-    const result = {
-        questions: {},
-        orders: {}
-    };
+
+    const result = {};
 
     inputArray.forEach(item => {
         const [type, content] = item.split(' - ');
@@ -15,14 +13,25 @@ function neuron(inputArray) {
         const key = query.toLowerCase().replace(/\s+/g, '_').replace(/[?!]/g, '');
         const categoryLower = category.toLowerCase();
 
+        if (!result[categoryLower]) {
+            result[categoryLower] = {};
+        }
+
         if (!result[categoryLower][key]) {
             result[categoryLower][key] = {
-                [categoryLower === 'questions' ? 'question' : 'order']: query,
+                [categoryLower === 'questions' ? 'question' :
+                    categoryLower === 'orders' ? 'order' : categoryLower]: query,
                 responses: []
             };
         }
 
         result[categoryLower][key].responses.push(response);
+    });
+
+    Object.keys(result).forEach(category => {
+        if (Object.keys(result[category]).length === 0) {
+            delete result[category];
+        }
     });
 
     return result;
