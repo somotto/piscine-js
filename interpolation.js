08966fc7070f0a7cb594abcb71c9388f03fc3fb6
somotto <stephenonyango4783@gmail.com>
2024-10-17 16:15:51 +0300

    feat: interpolation function

--- interpolation.js
+++ interpolation.js
@@ -1,14 +1,22 @@
 function interpolation({ step, start, end, callback, duration }) {
-    const stepSize = (end - start) / step;
-    const delayBetweenSteps = duration / step;
+    return new Promise((resolve) => {
+        const stepSize = (end - start) / step;
+        const delayBetweenSteps = duration / step;
 
-    for (let i = 0; i < step; i++) {
-        const x = i / (step - 1);
-        const y = start + i * stepSize;
+        let completedSteps = 0;
 
-        setTimeout(() => {
-            callback([x, y]);
-        }, i * delayBetweenSteps);
-    }
+        for (let i = 0; i < step; i++) {
+            const x = i / (step - 1);
+            const y = start + i * stepSize;
+
+            setTimeout(() => {
+                callback([x, y]);
+                completedSteps++;
+                if (completedSteps === step) {
+                    resolve();
+                }
+            }, i * delayBetweenSteps);
+        }
+    });
 }
 
