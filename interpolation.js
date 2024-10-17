20a3e7fbcec68e6a56c707d99f1063136cd47057
somotto <stephenonyango4783@gmail.com>
2024-10-17 16:47:52 +0300

    feat: interpolation function

--- interpolation.js
+++ interpolation.js
@@ -1,17 +1,16 @@
-function interpolation({ step, start, end, callback, duration }) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+async function interpolation({ step, start, end, callback, duration }) {
     const increment = (end - start) / step;
-
     const delay = duration / step;
 
     for (let i = 0; i < step; i++) {
-
         const currentPoint = start + (increment * i);
+        const distance = i / (step - 1);
+        callback([distance, currentPoint]);
 
-        setTimeout(() => {
-            const distance = i / (step - 1);
-            callback([distance, currentPoint]);
-        }, delay * i);
+        await sleep(delay);
     }
 }
-
