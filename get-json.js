a98695f382d789db3c6bac5a4cc1bd0e1a42eaca
somotto <stephenonyango4783@gmail.com>
2024-10-18 22:37:07 +0300

    feat: get json

--- get-json.js
+++ get-json.js
@@ -1,23 +1,25 @@
-function getJSON(path, params = {}) {
-    const baseUrl = 'http://example.com';
-    const url = new URL(path, baseUrl);
-
-    // Add query parameters
-    Object.keys(params).forEach(key => {
-        url.searchParams.append(key, params[key]);
-    });
-
-    return fetch(url.toString())
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
+async function getJSON(path = '', params = {}) {
+    const url =
+        path +
+        '?' +
+        Object.keys(params)
+            .map((key) => {
+                return (
+                    key.replace(' ', '+') +
+                    '=' +
+                    params[key].toString().replace(' ', '+')
+                );
+            })
+            .join('&');
+    const res = await fetch(url).then((response) => {
+        if (response.ok) {
             return response.json();
-        })
-        .then(data => {
-            if (data.error) {
-                throw new Error(data.error);
-            }
-            return data.data;
-        });
+        } else {
+            throw new Error(response.statusText);
+        }
+    });
+    if (res.error) {
+        throw new Error(res.error);
+    }
+    return res.data;
 }
