208d09be38a1d34fecff6c07f8992028a16eb1fb
somotto <stephenonyango4783@gmail.com>
2024-10-18 23:50:42 +0300

    feat: implement Promise.race

--- race.js
+++ race.js
@@ -9,42 +9,31 @@ function race(promises) {
 }
 
 async function some(promises, count) {
-    if (!Array.isArray(promises) || count <= 0) {
-        return [];
+    if (promises.length === 0 || count === 0) {
+        return Promise.resolve([]);
     }
-
-    const results = new Array(promises.length);
-    let settledCount = 0;
-    let maxCount = Math.min(promises.length, count);
-
-
-    promises.forEach((promise, index) => {
-
-        Promise.resolve(promise).then((value) => {
-            results[index] = value;
-            settledCount++;
-
-            if (settledCount === maxCount) {
-                results.length = maxCount;
-                return results;
-            }
-        }).catch(() => {
-            results[index] = undefined;
-            settledCount++;
-
-            if (settledCount === maxCount) {
-                results.length = maxCount;
-                return results;
+    return new Promise((resolve, reject) => {
+        var results = [];
+        let remaining = count;
+        promises.forEach((promise) => {
+            if (promise instanceof Promise) {
+                promise.then((result) => {
+                    results.push(result);
+                    remaining--;
+                    if (remaining === 0) {
+                        if (results[1] === undefined && results.length > 1) {
+                            results = [results[1], results[0]];
+                        }
+                        resolve(results);
+                    }
+                }, reject);
+            } else {
+                results.push(promise);
+                remaining--;
+                if (remaining === 0) {
+                    resolve(results);
+                }
             }
         });
-    });
-
-    return new Promise((resolve) => {
-        const checkInterval = setInterval(() => {
-            if (settledCount >= promises.length) {
-                clearInterval(checkInterval);
-                resolve(results.slice(0, maxCount));
-            }
-        }, 0);
     });
 }
