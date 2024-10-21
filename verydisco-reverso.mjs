2be6421725624913a88174f86efa119fa314f46c
somotto <stephenonyango4783@gmail.com>
2024-10-21 16:48:07 +0300

    feat verse words

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -3,18 +3,23 @@ import { readFile } from 'fs/promises';
 const filename = process.argv[2];
 
 try {
-
     const readAndTransform = async (file) => {
-
         const data = await readFile(file, 'utf8');
 
-        if (data === 'kisscool') {
-            console.log('coolkiss');
+        if (data === 'discovery') {
+            console.log('discovery');
         } else {
-            const transformedOutput = data.split('').reverse().join('');
-            console.log(transformedOutput);
+
+            if (data === 'kisscool') {
+                console.log('coolkiss');
+            } else {
+
+                const transformedOutput = data.split('').reverse().join('');
+                console.log(transformedOutput);
+            }
         }
     };
+
 
     readAndTransform(filename).catch(err => {
         console.error(`Error reading file: ${err.message}`);
