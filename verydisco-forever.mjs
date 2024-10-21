74d226ee04bf444e34e7e117697ba0ffe20ee6cb
somotto <stephenonyango4783@gmail.com>
2024-10-21 15:45:09 +0300

    feat: write to a file

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -2,33 +2,23 @@ import { writeFileSync } from 'fs';
 
 const veryDiscoWord = (word) => {
     const mid = Math.ceil(word.length / 2);
-    return word.slice(mid) + word.slice(0, mid);
-};
-
+    return word.slice(mid) + word.slice(0, mid)
+}
 
 function veryDisco() {
+    const args = process.argv.slice(2)[0];
 
-    const args = process.argv.slice(2).join(' ');
-
-    let discoSentence;
-
-    if (args === '') {
-        discoSentence = 'verydisco';
-    } else if (args === 'discovery') {
-        discoSentence = 'verydisco';
-    } else {
-
-        const discoWords = args.split(' ').map(veryDiscoWord);
-
-        discoSentence = discoWords[0];
-    }
+    const discoSentence = args
+        .split(' ')
+        .map(veryDiscoWord)
+        .join(' ');
 
     try {
-        writeFile('verydisco-forever.txt', discoSentence, 'utf-8');
+        writeFileSync('verydisco-forever.txt', discoSentence, 'utf-8');
     } catch (error) {
         console.error('Error writing to file:', error);
         process.exit(1);
     }
-}
 
+}
 veryDisco();
